eaa74cc591958c50359bdda343c337985e5069a4
Christoffer Fink <christofferfink@gmail.com>
2013-10-11 14:13:37 +0200

    Added code mapping classes

--- inputpy/mapping.py
+++ inputpy/mapping.py
@@ -2,20 +2,29 @@
 mapping.py
 
 A module for handling code mappings.
+Note that this module has state.
 
 :copyright: (c) 2013 by Christoffer Fink.
 :license: MIT. See LICENSE for details.
 """
 import importlib
 
-mappings = {}
+__all__ = ('getType', 'Mapping', 'CodeMapping', )
 
-def getType(typeString):
+mappings = {}
+types = {}
+
+def __load(typeString):
     """
     Return the type referred to by a string naming the type, including any
     package and module names.
     While the use of plain built-in type names ('int') is discouraged
     ('builtins.int' should be used instead), both cases are supported.
+
+    This is a low level helper function. It does not do any caching, so it
+    will always import modules.
+    This function is as pure as a function that interacts with the file
+    system can be.
     """
     typeName = typeString
     moduleName = 'builtins'
@@ -27,3 +36,91 @@ def getType(typeString):
 
     module = importlib.import_module(moduleName)
     return module.__dict__[typeName]
+
+def getType(typeString):
+    """
+    Return a type referred to by a string. Modules will be imported as
+    necessary. Redundant imports are avoided by caching the results.
+    """
+    if typeString not in types:
+        types[typeString] = __load(typeString)
+    return types[typeString]
+
+class Mapping:
+    def __init__(self, id, type, constructor=None, set=None, get=None):
+        self.id = id
+        self.type = type
+        self.constructor = constructor
+        self.setter = set
+        self.getter = get
+
+        if constructor is not None:
+            self.dep = tuple(constructor.split())
+        else:
+            self.dep = ()
+
+    def getId(self):
+        return self.id
+
+    def getType(self):
+        return self.type
+
+    def getTypeName(self):
+        return self.type
+
+    def getDependencies(self):
+        return self.dep
+
+    def getConstructor(self):
+        return self.constructor
+
+    def getSetter(self):
+        return self.setter
+
+    def getGetter(self):
+        return self.getter
+
+    def getParameters(self):
+        return {
+            'id': self.id, 'type': self.type, 'constructor': self.constructor,
+            'set': self.setter, 'get': self.getter,
+        }
+
+    @staticmethod
+    def makeDirect(paramId, mapping):
+        params = mapping.getParameters()
+        params['id'] = paramId
+        return Mapping(**params)
+
+class CodeMapping:
+    """
+    Objects of this class act as databases of code mappings.
+    The class uses a bit of internal magic to simplify handling of the
+    different mappings, but the original information should still be
+    reproducible.
+    """
+    def __init__(self, paramMappings, mappingTypes):
+        """
+        Regular mappings (corresponding to a "Mapping" element) and
+        mapping types (corresponding to a "MappingType" element) are
+        handled separately to make it possible to accurately export the
+        object to XML if needed.
+        """
+        self.__paramMappings = paramMappings
+        self.__mappingTypes = mappingTypes
+
+        types = {m.getId(): m for m in mappingTypes}
+
+        # Update the mapping so that every parameter is associated to a mapping.
+        # If the type matches one of the mapping types, then a new mapping is
+        # created. The same type and initialization information is used, but
+        # now it is directly mapped from a parameter ID.
+        self.__mappings = {m.getId(): m for m in paramMappings}
+        self.__mappings.update({
+            m.getId(): Mapping.makeDirect(m.getId(), types[m.getTypeName()])
+            for m in paramMappings if m.getTypeName() in types
+        })
+
+    # Always returns a full/direct mapping.
+    def getMapping(self, id):
+        return self.__mappings[id]
--- test/test_mapping.py
+++ test/test_mapping.py
@@ -2,6 +2,9 @@ import unittest
 from inputpy import mapping
 from test import SomeStructural
 from test.types.simple import EmptyClass
+from test.types.geo import Point
+from inputpy.mapping import Mapping
+from inputpy.mapping import CodeMapping
 
 class TestMapping(unittest.TestCase):
     def testGetBuiltinTypes(self):
@@ -16,9 +19,65 @@ class TestMapping(unittest.TestCase):
         tests = [
             ('test.SomeStructural', SomeStructural),
             ('test.types.simple.EmptyClass', EmptyClass),
+            ('test.types.geo.Point', Point),
         ]
         for (name, result) in tests:
             self.assertIs(mapping.getType(name), result)
+
+    def testCreateMapping(self):
+        typeString = 'test.types.geo.Point'
+        paramId = 'ParamId'
+        m = Mapping(paramId, typeString, 'X Y')
+        self.assertEqual(paramId, m.getId())
+        self.assertEqual(typeString, m.getType())
+        self.assertEqual(('X', 'Y'), m.getDependencies())
+
+    def testCreateSimplMapping(self):
+        typeString = 'test.types.simple.EmptyClass'
+        paramId = 'ParamId'
+        m = Mapping(paramId, typeString)
+        self.assertEqual(paramId, m.getId())
+        self.assertEqual(typeString, m.getType())
+        self.assertEqual((), m.getDependencies())
+
+    def testMakeDirectMapping(self):
+        typeMapping = Mapping('PointType', 'test.types.geo.Point', 'X Y')
+        pointMapping = Mapping.makeDirect('Point', typeMapping)
+        self.assertEqual(pointMapping.getTypeName(), typeMapping.getTypeName())
+        self.assertEqual(pointMapping.getConstructor(), typeMapping.getConstructor())
+        self.assertEqual('Point', pointMapping.getId())
+
+    def testGetMappingFromCodeMapping(self):
+        # Note that we are dealing with two different 'Point' IDs.
+        # One names a code mapping (a mapping type) and the other names
+        # a parameter (a structured parameter).
+        # There does not exist a direct mapping from parameter 'Point'
+        # to type 'test.types.geo.Point'. Yet, when the full code mapping
+        # object has been created, such a direct mapping will be returned.
+        pointType = Mapping('Point', 'test.types.geo.Point', 'X Y')
+        pointMapping = Mapping('Point', 'Point')
+        emptyMapping = Mapping('Empty', 'test.types.simple.EmptyClass')
+        mapping = CodeMapping([pointMapping, emptyMapping], [pointType])
+        # The point of the test is that the difference of Mapping and
+        # MappingType have been hidden and 'Empty' and 'Point' are treated
+        # as if they were both defined with a Mapping.
+        empty = mapping.getMapping('Empty')
+        self.assertEqual('Empty', empty.getId())
+        self.assertEqual('test.types.simple.EmptyClass', empty.getTypeName())
+        point = mapping.getMapping('Point')
+        self.assertEqual('Point', point.getId())
+        self.assertEqual('test.types.geo.Point', point.getTypeName())
+
+    # In the test above, the parameter name and the type name match ('Point').
+    # Here the names are different to underscore that a transformation is
+    # really occurring (pointType could just have been reused above).
+    def testGetMappingFromCodeMappingWithDifferentNames(self):
+        pointType = Mapping('PointType', 'test.types.geo.Point', 'X Y')
+        pointMapping = Mapping('Point', 'PointType')
+        mapping = CodeMapping([pointMapping], [pointType])
+        point = mapping.getMapping('Point')
+        self.assertEqual('Point', point.getId())
+        self.assertEqual('test.types.geo.Point', point.getTypeName())
 
 if __name__ == '__main__':
     unittest.main()
