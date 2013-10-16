287cb577a305899df78ceaad0aa9b1720fe4dee4
Christoffer Fink <christofferfink@gmail.com>
2013-10-16 14:34:52 +0200

    Using mapping equality feature to test factories
    Code mapping factories can now be tested the same way that
    design space factories can be tested, thanks to __eq__.

--- test/test_factories.py
+++ test/test_factories.py
@@ -5,28 +5,24 @@ from test.tools import PresetDesignSpaceFactory
 
 class TestFactories(unittest.TestCase):
     def testPresetCodeMappingFactory(self):
-        config = 'triangleMapping.xml'
+        configs = (
+            'triangleMapping.xml',
+        )
         factories = (
             XMLFactory.getCodeMapping,
             PresetCodeMappingFactory.getCodeMapping,
         )
 
+        results = []
         for factory in factories:
-            mapping = factory(config)
+            results.append({config: factory(config) for config in configs})
 
-            outerP1 = mapping.getMapping('P1')
-            nestedP1 = mapping.getMapping('T1.P1')
-            triangle = mapping.getMapping('T1')
+        first = results[0]
+        for result in results[1:]:
+            for (k,v) in result.items():
+                msg = 'Import did not match for %s' % (k)
+                self.assertEqual(v, first[k], msg=msg)
 
-            self.assertEqual('P1', outerP1.getId())
-            self.assertEqual('test.types.geo.Point', outerP1.getTypeName())
-            self.assertEqual('T1.P1', nestedP1.getId())
-            self.assertEqual('test.types.geo.Point', nestedP1.getTypeName())
-            self.assertEqual('T1', triangle.getId())
-            self.assertEqual('test.types.geo.Triangle', triangle.getTypeName())
-            self.assertEqual('P1 P2 P3', triangle.getConstructor())
-
-            self.assertCountEqual(('X', 'Y'), outerP1.getDependencies())
 
     # This test is pretty crude, but maybe it's good enough?
     # Should it test design space/param store/code mapping for equality?
