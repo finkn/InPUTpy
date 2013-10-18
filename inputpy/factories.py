1bacdff7bb8877d54d2815837a09fe85c78dddd5
Christoffer Fink <christofferfink@gmail.com>
2013-10-18 18:24:18 +0200

    Cleanup and bug fix in XML factories
    - Various cleanup.
    - getParamStore didn't use the code mapping.

--- inputpy/factories.py
+++ inputpy/factories.py
@@ -4,28 +4,30 @@
 """
 import xml.etree.ElementTree as et
 import inputpy.param as param
+import inputpy.util as util
 from inputpy.designspace import DesignSpace
-from inputpy.mapping import Mapping, CodeMapping
+from inputpy.mapping import Mapping, CodeMapping, NULL_CODE_MAPPING
+from inputpy.q import *
 
 class XMLFactory:
     @staticmethod
-    def getParameter(element, parentId=None, codeMapping=None):
-        # Does not yet handle nested parameters.
+    def __getParamArgs(element, parentId=None):
         args = element.attrib
-
-        if element.tag.endswith('SParam'):
-            args['type'] = 'SParam'
-            paramId = args['id']
-            if parentId is None:
-                nextParent = paramId
-            else:
-                nextParent = parentId + '.' + paramId
+        if element.tag.endswith(SPARAM):
+            args['type'] = SPARAM
+            paramId = args[ID_ATTR]
+            nextParent = util.absolute(parentId, paramId)
             args['nested'] = [
-                XMLFactory.getParameter(e, nextParent) for e in element
+                XMLFactory.__getParamArgs(e, nextParent) for e in element
             ]
 
         args['parentId'] = parentId
-        return param.getParameter(**args)
+        return args
+
+    @staticmethod
+    def getParameter(element, codeMapping=None):
+        cm = codeMapping or NULL_CODE_MAPPING
+        return param.paramFactory(XMLFactory.__getParamArgs(element), cm)
 
     @staticmethod
     def getMapping(element):
@@ -38,26 +40,26 @@ class XMLFactory:
         mappings = []
         mappingTypes = []
         for e in root:
-            if e.tag.endswith('Mapping'):
+            if e.tag.endswith(MAPPING):
                 mappings.append(factory(e))
-            elif e.tag.endswith('MappingType'):
+            elif e.tag.endswith(MAPPING_TYPE):
                 mappingTypes.append(factory(e))
         return CodeMapping(mappings, mappingTypes)
 
     @staticmethod
     def getParamStore(root, codeMapping=None, paramFactory=None):
-        # Does not yet handle nested parameters.
         factory = paramFactory or XMLFactory.getParameter
-        return param.ParamStore([factory(e) for e in root])
+        cm = codeMapping
+        return param.ParamStore([factory(e, codeMapping=cm) for e in root])
 
     @staticmethod
     def getDesignSpace(fileName, codeMappingFactory=None, psFactory=None):
         factory = psFactory or XMLFactory.getParamStore
         cmFactory = codeMappingFactory or XMLFactory.getCodeMapping
         root = et.parse(fileName).getroot()
-        codeMapping = root.attrib.get('mapping')
+        codeMapping = root.attrib.get(MAPPING_ATTR)
         if codeMapping is not None:
             codeMapping = cmFactory(codeMapping)
         ps = factory(root, codeMapping=codeMapping)
-        spaceId = root.get('id')
+        spaceId = root.get(ID_ATTR)
         return DesignSpace(ps, spaceId, fileName)
