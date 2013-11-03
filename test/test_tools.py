df1cb314e2b63bd1186e14630ddca5d913cc4583
Christoffer Fink <christofferfink@gmail.com>
2013-11-03 14:24:45 +0100

    Added more test tools

--- test/test_tools.py
+++ test/test_tools.py
@@ -6,6 +6,8 @@ import unittest
 import random
 from test.tools import *
 
+# TODO:
+# Make a more reliable generator that can replace random.
 class TestTools(unittest.TestCase):
 
     # --- checkVariability tests ---
@@ -71,6 +73,75 @@ class TestTools(unittest.TestCase):
         for (sizes, array) in tests:
             msg = 'sizes %s matched array %s' % (sizes, array)
             self.assertFalse(checkArrayDimensions(sizes, array), msg=msg)
+
+
+    # --- check(Any|All)ValuesMatch tests ---
+    def testCheckAnyValueMatchesForAnyEmptyArgumentShouldFail(self):
+        self.assertFalse(checkAnyValueMatches([], []))
+        self.assertFalse(checkAnyValueMatches([], [1,2,3]))
+        self.assertFalse(checkAnyValueMatches([1,2,3], []))
+
+    def testCheckAllValuesMatchForEmptyValuesShouldSucceed(self):
+        self.assertTrue(checkAllValuesMatch([], [1,2,3]))
+        self.assertTrue(checkAllValuesMatch([], []))
+
+    def testCheckAllValuesMatchForEmptyExpectedShouldFail(self):
+        self.assertFalse(checkAllValuesMatch([1,2,3], []))
+
+    def testCheckAnyAndAllValuesShouldMatchWithSingleElement(self):
+        self.assertTrue(checkAnyValueMatches([1], [1]))
+        self.assertTrue(checkAllValuesMatch([1], [1]))
+
+    def testCheckAnyValueMatchesShouldMatchWithMixedElements(self):
+        self.assertTrue(checkAnyValueMatches([1,2,3], ['a',2,False]))
+
+    def testCheckAllValuesMatchShouldNotMAtchWithMixedElements(self):
+        self.assertFalse(checkAllValuesMatch([1,2,3], ['a',2,False]))
+
+
+    # --- check generated values matching tests ---
+    def testCheckGeneratesAnyExpectedShouldFailIfNoneMatch(self):
+        f = lambda: random.randint(10,20)
+        expected = [1,2,3,4,5]
+        self.assertFalse(checkGeneratesAnyExpected(f, expected))
+
+    # Only 2 will be generated, but it matches, and that's enough.
+    def testCheckGeneratesAnyExpectedShouldSucceedIfAnyMatch(self):
+        f = lambda: 2
+        expected = [1,2,3,4,5]
+        self.assertTrue(checkGeneratesAnyExpected(f, expected))
+
+    def testCheckGeneratesAllExpectedWithTooFewIterationsShouldFail(self):
+        f = lambda: random.randint(1,5)
+        expected = [1,2,3,4,5]
+        with self.assertRaises(AssertionError):
+            self.assertFalse(checkGeneratesAllExpected(f, expected, 4))
+
+    # One value is missing from the range, so it's impossible to generate all.
+    def testCheckGeneratesAllExpectedShouldFailIfAnyMismatch(self):
+        f = lambda: random.randint(1,4)
+        expected = [1,2,3,4,5]
+        self.assertFalse(checkGeneratesAllExpected(f, expected))
+
+    # 3 does not match, but that is irrelevant as long as 1 and 2 occur.
+    def testCheckGeneratesAllExpectedShouldSucceedIfAllAreMatched(self):
+        f = lambda: random.randint(1,3)
+        expected = [1,2]
+        self.assertTrue(checkGeneratesAllExpected(f, expected))
+
+    # 1 and 2 match, but 3 (which will probably be generated) does not.
+    def testCheckGeneratesOnlyExpectedShouldFailIfAnyMismatch(self):
+        f = lambda: random.randint(1,3)
+        expected = [1,2]
+        self.assertFalse(checkGeneratesOnlyExpected(f, expected, 20))
+
+    # There are not enough iterations to generate all expected values, but we
+    # don't need to. What is important is that no non-matching values occur.
+    def testCheckGeneratesOnlyExpectedShouldSucceedIfNoMismatch(self):
+        f = lambda: random.randint(1,10)
+        expected = [1,2,3,4,5,6,7,8,9,10]
+        self.assertTrue(checkGeneratesOnlyExpected(f, expected, 9))
+
 
 if __name__ == '__main__':
     unittest.main()
--- test/tools.py
+++ test/tools.py
@@ -41,3 +41,65 @@ def checkArrayDimensions(sizes, array):
         if not checkArrayDimensions(sizes[1:], element):
             return False    # Check sub-arrays recursively.
     return True
+
+
+# Note that these two functions can be used in two different ways, simply
+# by flipping the order of their arguments. The values can either be
+# matched against expected values, or the other way around. Both are useful.
+
+def checkAnyValueMatches(values, expected):
+    """
+    Return whether any of the values match an expected value.
+    The return value is True if and only if there exists some value v
+    such that v is one of the expected values.
+    """
+    return any([v in expected for v in values])
+
+def checkAllValuesMatch(values, expected):
+    """
+    Return whether all of the values match an expected value.
+    The return value is True if and only if there does not exist some
+    value v such that v is not one of the expected values.
+    """
+    return all([v in expected for v in values])
+
+
+def checkGeneratesAnyExpected(f, expected, iterations=10):
+    """
+    Return True if the function f generates one of the expected values.
+    """
+    for i in range(iterations):
+        if f() in expected:
+            return True
+    return False
+
+def checkGeneratesAllExpected(f, expected, iterations=None):
+    """
+    Return True if the function f generates all of the expected values.
+    The number of iterations is proportional to the number of values.
+    If given, iterations must be >= the number of expected values.
+    """
+    if iterations is None:
+        iterations = len(expected) * 10
+    else:
+        msg = 'Must generate at least as many values as are expected'
+        assert iterations > len(expected), msg
+
+    expected = list(expected)
+    for i in range(iterations):
+        v = f()
+        if v in expected:
+            expected.remove(v)
+        if len(expected) == 0:
+            return True
+    return False
+
+def checkGeneratesOnlyExpected(f, expected, iterations=10):
+    """
+    Return False as soon as f generates a value that does not match any of
+    the expected values.
+    """
+    for i in range(iterations):
+        if f() not in expected:
+            return False
+    return True
