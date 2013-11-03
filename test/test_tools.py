9b6434aedbeb792c10db5703ad79c67d14225dc1
Christoffer Fink <christofferfink@gmail.com>
2013-11-03 15:57:41 +0100

    Removed most check* functions from test.tools

--- test/test_tools.py
+++ test/test_tools.py
@@ -13,40 +13,30 @@ class TestTools(unittest.TestCase):
 
     # ----- checkVariability tests -----
 
-    def testCheckVariabilityWithOneIterationShouldFail(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            with self.assertRaises(AssertionError):
-                checkVariability(None, iterations=1)
+    def testAssertVariabilityWithOneIterationShouldFail(self):
+        with self.assertRaises(AssertionError):
+            assertVariability(None, iterations=1)
 
-    def testCheckVariabilityWithConstantValueShouldFail(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            self.assertFalse(checkVariability(lambda: 1))
+    def testAssertVariabilityWithConstantValueShouldFail(self):
+        with self.assertRaises(AssertionError):
+            assertVariability(lambda: 1)
 
-    def testCheckVariabilityWithRandomValueShouldSucceed(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            self.assertTrue(checkVariability(lambda: random.randint(1, 10000)))
+    def testAssertVariabilityWithRandomValueShouldSucceed(self):
+        assertVariability(lambda: random.randint(1, 10000))
 
 
     # ----- checkConstancy tests -----
 
-    def testCheckConstancyWithOneIterationShouldFail(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            with self.assertRaises(AssertionError):
-                checkConstancy(None, iterations=1)
+    def testAssertConstancyWithOneIterationShouldFail(self):
+        with self.assertRaises(AssertionError):
+            assertConstancy(None, iterations=1)
 
-    def testCheckConstancyWithConstantValueShouldFail(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            self.assertTrue(checkConstancy(lambda: 1))
+    def testAssertConstancyWithConstantValueShouldSucceed(self):
+        assertConstancy(lambda: 1)
 
-    def testCheckConstancyWithRandomValueShouldSucceed(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            self.assertFalse(checkConstancy(lambda: random.randint(1, 10000)))
+    def testAssertConstancyWithRandomValueShouldFail(self):
+        with self.assertRaises(AssertionError):
+            assertConstancy(lambda: random.randint(1, 10000))
 
 
     # ----- checkArrayDimensions tests -----
@@ -58,9 +48,6 @@ class TestTools(unittest.TestCase):
             ((5,), [1, 1, 1, 1, 1]),
         )
         for (sizes, array) in tests:
-            with warnings.catch_warnings(record=False):
-                warnings.simplefilter('ignore')
-                self.assertTrue(checkArrayDimensions(sizes, array))
             assertMatchingArrayDimensions(sizes, array)
 
     def testSingleDimensionArrayWithWrongSizeShouldFail(self):
@@ -70,9 +57,6 @@ class TestTools(unittest.TestCase):
             ((1,), [1, 1, 1, 1, 1]),
         )
         for (sizes, array) in tests:
-            with warnings.catch_warnings(record=False):
-                warnings.simplefilter('ignore')
-                self.assertFalse(checkArrayDimensions(sizes, array))
             with self.assertRaises(AssertionError):
                 assertMatchingArrayDimensions(sizes, array)
 
@@ -84,10 +68,6 @@ class TestTools(unittest.TestCase):
             ((1, 2, 2), [[[1, 1], [1, 1]]]),
         )
         for (sizes, array) in tests:
-            with warnings.catch_warnings(record=False):
-                warnings.simplefilter('ignore')
-                msg = 'sizes %s did not match array %s' % (sizes, array)
-                self.assertTrue(checkArrayDimensions(sizes, array), msg=msg)
             assertMatchingArrayDimensions(sizes, array)
 
     def testMultiDimensionalArrayWithWrongSizesShouldFail(self):
@@ -97,10 +77,6 @@ class TestTools(unittest.TestCase):
             ((1, 2, 2), [[[1], [1]]]),  # Actually (1, 2, 1)
         )
         for (sizes, array) in tests:
-            with warnings.catch_warnings(record=False):
-                warnings.simplefilter('ignore')
-                msg = 'sizes %s matched array %s' % (sizes, array)
-                self.assertFalse(checkArrayDimensions(sizes, array), msg=msg)
             with self.assertRaises(AssertionError):
                 assertMatchingArrayDimensions(sizes, array)
 
@@ -140,65 +116,6 @@ class TestTools(unittest.TestCase):
         with warnings.catch_warnings(record=False):
             warnings.simplefilter('ignore')
             self.assertFalse(checkAllValuesMatch([1,2,3], ['a',2,False]))
-
-
-    # ----- check generated values matching tests -----
-
-    def testCheckGeneratesAnyExpectedShouldFailIfNoneMatch(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(10,20)
-            expected = [1,2,3,4,5]
-            self.assertFalse(checkGeneratesAnyExpected(f, expected))
-
-    # Only 2 will be generated, but it matches, and that's enough.
-    def testCheckGeneratesAnyExpectedShouldSucceedIfAnyMatch(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: 2
-            expected = [1,2,3,4,5]
-            self.assertTrue(checkGeneratesAnyExpected(f, expected))
-
-    def testCheckGeneratesAllExpectedWithTooFewIterationsShouldFail(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(1,5)
-            expected = [1,2,3,4,5]
-            with self.assertRaises(AssertionError):
-                self.assertFalse(checkGeneratesAllExpected(f, expected, 4))
-
-    # One value is missing from the range, so it's impossible to generate all.
-    def testCheckGeneratesAllExpectedShouldFailIfAnyMismatch(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(1,4)
-            expected = [1,2,3,4,5]
-            self.assertFalse(checkGeneratesAllExpected(f, expected))
-
-    # 3 does not match, but that is irrelevant as long as 1 and 2 occur.
-    def testCheckGeneratesAllExpectedShouldSucceedIfAllAreMatched(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(1,3)
-            expected = [1,2]
-            self.assertTrue(checkGeneratesAllExpected(f, expected))
-
-    # 1 and 2 match, but 3 (which will probably be generated) does not.
-    def testCheckGeneratesOnlyExpectedShouldFailIfAnyMismatch(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(1,3)
-            expected = [1,2]
-            self.assertFalse(checkGeneratesOnlyExpected(f, expected, 20))
-
-    # There are not enough iterations to generate all expected values, but we
-    # don't need to. What is important is that no non-matching values occur.
-    def testCheckGeneratesOnlyExpectedShouldSucceedIfNoMismatch(self):
-        with warnings.catch_warnings(record=False):
-            warnings.simplefilter('ignore')
-            f = lambda: random.randint(1,10)
-            expected = [1,2,3,4,5,6,7,8,9,10]
-            self.assertTrue(checkGeneratesOnlyExpected(f, expected, 9))
 
 
     # ----- assert generated values matching tests -----
--- test/tools.py
+++ test/tools.py
@@ -9,35 +9,6 @@ Tools that are useful for writing unit tests for InPUT.
 
 import warnings
 
-# TODO:
-# Remove all the check* functions once all of them have assert* replacements
-# and they are not longer used elsewhere.
-
-def checkVariability(f, iterations=10):
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    assert iterations > 1, '1 iteration makes no sense!'
-    values = []
-    for i in range(iterations):
-        value = f()
-        if value not in values and len(values) > 0:
-            return True     # Found a new value.
-        else:
-            values.append(value)
-    return False            # No variation after N iterations.
-
-def checkConstancy(f, iterations=10):
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    assert iterations > 1, '1 iteration makes no sense!'
-    values = []
-    for i in range(iterations):
-        value = f()
-        if value not in values and len(values) > 0:
-            return False    # Found a new value.
-        else:
-            values.append(value)
-    return True             # No variation after N iterations.
-
-
 def assertVariability(f, iterations=10):
     """ Assert that f will generate at least one unique value. """
     assert iterations > 1, '1 iteration makes no sense!'
@@ -65,18 +36,6 @@ def assertConstancy(f, iterations=10):
     return True             # No variation after N iterations.
 
 
-def checkArrayDimensions(sizes, array):
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    size = sizes[0]
-    if size != len(array):
-        return False        # Current dimension does not match.
-    if len(sizes) == 1:
-        return True         # No more dimensions. Done.
-    for element in array:
-        if not checkArrayDimensions(sizes[1:], element):
-            return False    # Check sub-arrays recursively.
-    return True
-
 def assertMatchingArrayDimensions(sizes, array):
     """
     Assert that the array dimensions match the given sizes.
@@ -99,6 +58,8 @@ def assertMatchingArrayDimensions(sizes, array):
 # by flipping the order of their arguments. The values can either be
 # matched against expected values, or the other way around. Both are useful.
 
+# TODO:
+# These two still need to be replaced assert versions (or possibly removed).
 def checkAnyValueMatches(values, expected):
     """
     Return whether any of the values match an expected value.
@@ -116,50 +77,6 @@ def checkAllValuesMatch(values, expected):
     """
     warnings.warn('use the assert version instead', DeprecationWarning)
     return all([v in expected for v in values])
-
-
-def checkGeneratesAnyExpected(f, expected, iterations=10):
-    """
-    Return True if the function f generates one of the expected values.
-    """
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    for i in range(iterations):
-        if f() in expected:
-            return True
-    return False
-
-def checkGeneratesAllExpected(f, expected, iterations=None):
-    """
-    Return True if the function f generates all of the expected values.
-    The number of iterations is proportional to the number of values.
-    If given, iterations must be >= the number of expected values.
-    """
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    if iterations is None:
-        iterations = len(expected) * 10
-    else:
-        msg = 'Must generate at least as many values as are expected'
-        assert iterations > len(expected), msg
-
-    expected = list(expected)
-    for i in range(iterations):
-        v = f()
-        if v in expected:
-            expected.remove(v)
-        if len(expected) == 0:
-            return True
-    return False
-
-def checkGeneratesOnlyExpected(f, expected, iterations=10):
-    """
-    Return False as soon as f generates a value that does not match any of
-    the expected values.
-    """
-    warnings.warn('use the assert version instead', DeprecationWarning)
-    for i in range(iterations):
-        if f() not in expected:
-            return False
-    return True
 
 
 def assertGeneratesAny(f, expected, iterations=10):
